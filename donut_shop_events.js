8058690b8f2106f745444091b6592f97ac2140ee
Mark Buchthal <mark.buchthal@gmail.com>
2015-04-07 19:35:59 -0700

    tweaked html form

--- donut_shop_events.js
+++ donut_shop_events.js
@@ -8,10 +8,10 @@ var locationForm = document.getElementById('new-location-form');
 
 var handleNoteSubmit = function(event) {
   event.preventDefault();
-  if (!(event.target.storelocation.value || event.target.mincustomers.value || event.target.maxcustomers.value || event.target.averagedonuts.value)) {             // this is to prevent a blank note from being made
+  if (!(event.target.storelocation.value || event.target.averagedonuts.value || event.target.mincustomers.value || event.target.maxcustomers.value)) {             // this is to prevent a blank note from being made
       return alert('note cannot be blank');
     }
-  var newStore = new Store(event.target.storelocation.value, event.target.mincustomers.value, event.target.maxcustomers.value, event.target.averagedonuts.value);            // this is to prevent a blank note from being made
+  var newStore = new Store(event.target.storelocation.value, event.target.averagedonuts.value, event.target.mincustomers.value, event.target.maxcustomers.value);            // this is to prevent a blank note from being made
       event.target.storelocation.value = null;
       event.target.averagedonuts.value = null;
       event.target.mincustomers.value = null;
@@ -19,6 +19,7 @@ var handleNoteSubmit = function(event) {
       // allStores.push(newStore);
       newStore.render();
       // tableList.appendChild(newStore.render());
+      console.log(newStore);
   };
 
 locationForm.addEventListener('submit', handleNoteSubmit);
