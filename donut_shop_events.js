fc33312571e3d8950c51ae4986dd4ee0d3abc595
Mark Buchthal <mark.buchthal@gmail.com>
2015-04-08 17:51:26 -0700

    add css file

--- donut_shop_events.js
+++ donut_shop_events.js
@@ -4,34 +4,19 @@ var tableList = document.getElementById('tablebody');
 var newRow = document.createElement('tr');
 var locationForm = document.getElementById('new-location-form');
 
-
-
 var handleNoteSubmit = function(event) {
   event.preventDefault();
   if (!(event.target.storelocation.value || event.target.averagedonuts.value || event.target.mincustomers.value || event.target.maxcustomers.value)) {             // this is to prevent a blank note from being made
       return alert('note cannot be blank');
     }
-  var newStore = new Store(event.target.storelocation.value, event.target.averagedonuts.value, event.target.mincustomers.value, event.target.maxcustomers.value);            // this is to prevent a blank note from being made
+  var newStore = new Store(event.target.storelocation.value, Number.parseInt(event.target.averagedonuts.value), Number.parseInt(event.target.mincustomers.value), Number.parseInt(event.target.maxcustomers.value));            // this is to prevent a blank note from being made
       event.target.storelocation.value = null;
       event.target.averagedonuts.value = null;
       event.target.mincustomers.value = null;
       event.target.maxcustomers.value = null;
-      // allStores.push(newStore);
       newStore.render();
-      // tableList.appendChild(newStore.render());
-      console.log(newStore);
   };
 
 locationForm.addEventListener('submit', handleNoteSubmit);
-
-// var updateRender = function() {
-
-//   tableList.innerHTML = '';
-//   allStores.forEach(function(store) {
-//     tableList.appendChild(store.render());
-
-//   });
-
-// }
 
 
